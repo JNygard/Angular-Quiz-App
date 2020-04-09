85547ae3b872ce34b137550f51157d4dbbbe50fe
JNygard <nygard600@gmail.com>
2020-04-09 21:37:43 +0300

    Initial UI sketched

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # QuizApp
 
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.0.
 
 ## Development server
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    QuizComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
